e7c06fb1c49509f93ea1cb1355aa5f7fd0cde159
gyildirimm <goktugyildirim22@gmail.com>
2021-06-07 23:45:33 +0300

    -discount API

---
+++ MicroServiceLearnProject/Services/Discount/MicroServiceProject.Services.Discount/IServices/IDiscountService.cs
@@ -0,0 +1,25 @@
+﻿using MicroServiceProject.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServiceProject.Services.Discount.IServices
+{
+    public interface IDiscountService
+    {
+
+        Task<Response<List<Models.Discount>>> GetAll();
+
+        Task<Response<Models.Discount>> GetById(int id);
+
+        Task<Response<NoContent>> Save(Models.Discount discount);
+
+        Task<Response<NoContent>> Update(Models.Discount discount);
+
+        Task<Response<NoContent>> Delete(int id);
+
+        Task<Response<Models.Discount>> GetByCodeAndUserId(string code, string userId);
+
+    }
+}
